ef96ad9a0b90b0d3a9da6f24c279ffcb8cfd57f6
Sheriff <realexcel2020@gmail.com>
2023-05-16 11:08:15 +0100

    Packaged the app completely

---
+++ src/backend/lambda_write.py
@@ -0,0 +1,29 @@
+import json
+import boto3
+from decimal import Decimal
+
+def lambda_handler(event, context):
+    visit_count : int = 0
+    body = json.loads(event['body'])
+    the_id = body['value']
+
+
+    dynamoDB = boto3.resource('dynamodb')
+    table_name = "veiw_count_terraform"
+    table = dynamoDB.Table(table_name)
+
+    response = table.get_item(Key={"id" : the_id})
+
+    if "Item" in response:
+        visit_count = response['Item']['veiw_count']
+
+    visit_count += 1
+
+    table.put_item(Item = {"id": the_id, "veiw_count" : visit_count})
+    
+    return_value = Decimal(visit_count)
+    
+    return {
+        'statusCode': 200,
+         'body' : json.dumps(return_value, default=str)
+    }
---
+++ tailwind.config.js
@@ -0,0 +1,30 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        lightblue : "#3c4240",
+        graycolor : "#2a3761"
+      }
+    },
+    screens: {
+      '2xl': {'max': '1535px'},
+      // => @media (max-width: 1535px) { ... }
+
+      'xl': {'max': '1279px'},
+      // => @media (max-width: 1279px) { ... }
+
+      'lg': {'max': '1023px'},
+      // => @media (max-width: 1023px) { ... }
+
+      'md': {'max': '767px'},
+      // => @media (max-width: 767px) { ... }
+
+      'sm': {'max': '639px'},
+      // => @media (max-width: 639px) { ... }
+      'big': {'min': '1023px'}
+    }
+  },
+  plugins: [],
+}
